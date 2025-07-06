61066fa49091a8154f17ba6b88542d1d48902650
heyadin <adinahmad003@gmail.com>
2025-07-07 01:54:38 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
-    <div>
-      NavBar
+    <div className='text-white  bg-[#151312] w-[100vw] h-[100vh] py-3'>
+      <Navbar/>
+      <div className='text-center'></div>
     </div>
   )
 }
