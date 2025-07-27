fd34747dcb2510a94b96253d5731e8e706bdf966
heyadin <adinahmad003@gmail.com>
2025-07-27 17:31:15 +0530

    completed ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import { HeroText } from './components/HeroText'
 import { Navbar } from './components/Navbar'
+import { Profile } from './components/Profile'
 
 function App() {
 
   return (
-    <div className='text-white  bg-[#151312] w-[100vw] h-[100vh] py-3'>
-      <Navbar/>
-      <div className='text-center'></div>
+    <div className='text-white  bg-[#151312] pt-3 h-screen overflow-hidden'>
+      <Navbar />
+      <div className='flex justify-center gap-30 my-20'>
+        <Profile />
+        <HeroText />
+      </div>
     </div>
   )
 }
